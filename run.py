737f58e57205dd50029167e0c3afc792fe350554
Nathan Van Gheem <vangheem@gmail.com>
2013-12-04 18:44:58 -0600

    add video and fix gpio refs

--- run.py
+++ run.py
@@ -5,6 +5,23 @@ import tornado.web
 import tornado.ioloop
 import os
 import json
+from RPi import GPIO
+
+
+# setup rpi
+GPIO.setmode(GPIO.BOARD)
+
+class channels(object):
+    forward = 21
+    reverse = 19
+    left = 11
+    right = 13
+
+
+GPIO.setup(channels.forward, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(channels.reverse, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(channels.left, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(channels.right, GPIO.OUT, initial=GPIO.LOW)
 
 _dir = os.path.dirname(os.path.realpath(__file__))
 template_path = os.path.join(_dir, 'templates')
@@ -12,20 +29,11 @@ template_path = os.path.join(_dir, 'templates')
 loader = tornado.template.Loader(template_path)
 
 
-def forward(stop=False, **kwargs):
-    print 'forward'
-
-
-def reverse(stop=False, **kwargs):
-    print 'reverse'
-
-
-def left(stop=False, **kwargs):
-    print 'left'
-
-
-def right(stop=False, **kwargs):
-    print 'right'
+def _action(channel, stop, **kwargs):
+    volt = GPIO.HIGH
+    if stop:
+        volt = GPIO.LOW
+    GPIO.output(channel, volt)
 
 
 class WebSocket(tornado.websocket.WebSocketHandler):
@@ -36,13 +44,17 @@ class WebSocket(tornado.websocket.WebSocketHandler):
     def on_message(self, message):
         data = json.loads(message)
         if data['dir'] == 'forward':
-            forward(**data)
+            _action(channels.reverse, True)
+            _action(channels.forward, **data)
         elif data['dir'] == 'reverse':
-            reverse(**data)
+            _action(channels.forward, True)
+            _action(channels.reverse, **data)
         elif data['dir'] == 'left':
-            left(**data)
+            _action(channels.right, True)
+            _action(channels.left, **data)
         elif data['dir'] == 'right':
-            right(**data)
+            _action(channels.left, True)
+            _action(channels.right, **data)
 
     def on_close(self):
         print 'closed'
@@ -63,5 +75,5 @@ application = tornado.web.Application([
 
 
 if __name__ == "__main__":
-    application.listen(8000)
+    application.listen(8000, '0.0.0.0')
     tornado.ioloop.IOLoop.instance().start()
